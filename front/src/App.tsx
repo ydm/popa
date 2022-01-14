8bfcca6f330a867288871897deeb7aa76e8969cd
Yordan Miladinov <jordanmiladinov@gmail.com>
2022-01-14 23:52:53 +0200

    setup

---
+++ front/src/App.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Empty } from "./definitions";
+
+import "./App.css";
+
+const App: React.VFC = (
+    _props: Empty,
+    _context?: unknown
+): React.ReactElement => {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
+};
+
+export default App;
